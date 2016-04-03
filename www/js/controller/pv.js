9bed24f0ab6097229f5972c0f4d55457328b2274
celine24 <peyrot.celine@gmail.com>
2016-04-03 18:37:36 +0200

    menu tab onglets css

--- www/js/controller/pv.js
+++ www/js/controller/pv.js
@@ -1,4 +1,7 @@
-pvs.controller("pvsController", function($scope) {
+pvs.controller("pvController", function($scope) {
 
-
+    $scope.relVisible = false;
+    $scope.show = function () {
+        $scope.relVisible = true;
+    };
 });
