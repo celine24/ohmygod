9d3ddffe61ad57940e8ac67dec121da016419380
celine24 <peyrot.celine@gmail.com>
2016-04-30 11:31:47 +0200

    fonction filtres epuree

--- public/js/controller/pvs.js
+++ public/js/controller/pvs.js
@@ -8,17 +8,10 @@ omgApp.controller("pvsCtrl", function($scope, API_URL, Pv, Group) {
         $scope.groups = resp;
     });
 
-    $scope.groupFilter = '';
-    $scope.activeGroup = null;
-
     $scope.activeFilters = [];
 
     $scope.groupF = function (group) {
-        if ($scope.groupFilter === '') {
-            $scope.groupFilter = group.id;
-            $scope.activeGroup = group.id;
-        }
-        else if ($scope.groupFilter === group.id) {
+        if ($scope.groupFilter === group.id) {
             $scope.groupFilter = '';
             $scope.activeGroup = null;
         }
@@ -29,38 +22,25 @@ omgApp.controller("pvsCtrl", function($scope, API_URL, Pv, Group) {
     };
 
     $scope.filter = function (value, filter, label, name) {
-        if (value === null) {
-            $scope[filter + 'Filter'] = value;
-            $scope[filter + 'Active'] = value;
-            angular.forEach($scope.activeFilters, function(index, key) {
-                //Object.keys(objet) pour retrouver le nom des propriétés
-                if (Object.keys(index)[0] === label) {
-                    $scope.activeFilters.splice($scope.activeFilters.indexOf(index),1);
-                }
-            });
-            $scope.activeFilters.push({[label]:name});
-        }
-        else if ($scope[filter + 'Filter'] === value) {
+        if ($scope[filter + 'Filter'] === value) {
             $scope[filter + 'Filter'] = '';
             $scope[filter + 'Active'] = null;
+            parseActiveFilters();
+        }
+        else {
+            $scope[filter + 'Filter'] = value;
+            $scope[filter + 'Active'] = value;
+            parseActiveFilters();
+            $scope.activeFilters.push({[label]:name});
+        }
+
+        function parseActiveFilters() {
             angular.forEach($scope.activeFilters, function(index, key) {
                 //Object.keys(objet) pour retrouver le nom des propriétés
                 if (Object.keys(index)[0] === label) {
                     $scope.activeFilters.splice($scope.activeFilters.indexOf(index),1);
                 }
             });
-        }
-        else {
-            angular.forEach($scope.activeFilters, function(index, key) {
-                //Object.keys(objet) pour retrouver le nom des propriétés
-                if (Object.keys(index)[0] === label) {
-                    $scope.activeFilters.splice($scope.activeFilters.indexOf(index), 1);
-                }
-
-            });
-            $scope.activeFilters.push({[label]:name});
-            $scope[filter + 'Filter'] = value;
-            $scope[filter + 'Active'] = value;
         }
     };
 
